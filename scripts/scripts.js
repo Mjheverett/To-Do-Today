36e23fce60588e20f7f2294f40d25259e20d84aa
Matthew Everett <matthew@mjheverett.com>
2020-08-28 15:36:19 -0400

    added getEvents function helper to create all DOM elements
    Co-authored-by: Harmony <harmonytrevena@gmail.com>
    Co-authored-by: Dylan Cooper <dylancooper155@gmail.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     document.getElementById('weatherHeader').style.display = "none";
     // document.getElementById('eventsColumns').style.display = "none";
+
+    // Display's Current Date
+    function displayDate(){
+        const currentDay = new Date();
+            let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            let day = days[currentDay.getDay()];
+        const currentDate = new Date();
+            let year = currentDate.getFullYear();
+            let date = currentDate.getDate();
+            let months =      ["January","February","March","April","May","June","July","August","September","October","November","December"];
+            let month = months[currentDate.getMonth()];
+        
+          // Use for Month inAPI
+        console.log(currentDate.getMonth());
+        
+        if (date<10){
+            date = "0" + date;
+        }
+        
+        const dateToday = document.getElementById('date');
+        dateToday.innerHTML = (day +" "+ month +" "+ date +", "+ year);
+        // Use for Date and Year in API
+        console.log(date);
+        console.log(year);
+    }
+    displayDate();
+    
 });
 
 // Initialize all div with carousel class
@@ -29,7 +56,7 @@ var carousels = bulmaCarousel.attach('.hero-carousel', {
 for(var i = 0; i < carousels.length; i++) {
 	// Add listener to  event
 	carousels[i].on('before:show', state => {
-		console.log(state);
+		// console.log(state);
 	});
 }
 
@@ -62,7 +89,7 @@ const getWeather = (currentCity) => {
 // Selector for current city
 atlantaButton.addEventListener('click', () => {
     currentCity = "Atlanta";
-    console.log("current city:", currentCity);
+    // console.log("current city:", currentCity);
     houstonButton.classList.remove('is-primary');
     seattleButton.classList.remove('is-primary');
     tampaButton.classList.remove('is-primary');
@@ -74,7 +101,7 @@ atlantaButton.addEventListener('click', () => {
 
 houstonButton.addEventListener('click', () => {
     currentCity = "Houston";
-    console.log("current city:", currentCity);
+    // console.log("current city:", currentCity);
     atlantaButton.classList.remove('is-primary');
     seattleButton.classList.remove('is-primary');
     tampaButton.classList.remove('is-primary');
@@ -86,7 +113,7 @@ houstonButton.addEventListener('click', () => {
 
 seattleButton.addEventListener('click', () => {
     currentCity = "Seattle";
-    console.log("current city:", currentCity);
+    // console.log("current city:", currentCity);
     atlantaButton.classList.remove('is-primary');
     houstonButton.classList.remove('is-primary');
     tampaButton.classList.remove('is-primary');
@@ -98,7 +125,7 @@ seattleButton.addEventListener('click', () => {
 
 tampaButton.addEventListener('click', () => {
     currentCity = "Tampa";
-    console.log("current city:", currentCity);
+    // console.log("current city:", currentCity);
     atlantaButton.classList.remove('is-primary');
     houstonButton.classList.remove('is-primary');
     seattleButton.classList.remove('is-primary');
@@ -110,119 +137,176 @@ tampaButton.addEventListener('click', () => {
 
 // ********** ********** Get Events Data ********** **********
 
+// Main function performs all calls to apis while managing refresh via Promise.all
 const getEventsData = (currentCity) => {
-    getBreweries(currentCity);
-    getConcerts(currentCity);
-    getSports(currentCity);
-    getFestivals(currentCity);
+    const concerts = getConcerts(currentCity);
+    const sports = getSports(currentCity);
+    const expos = getExpos(currentCity);
+    const community = getCommunity(currentCity);
+    const festivals = getFestivals(currentCity);
+    const performing = getPerformingArts(currentCity);
+    const breweries = getBreweries(currentCity);
+    const categoriesSelected = [breweries, community, concerts, expos, festivals, performing, sports];
+    Promise.all(categoriesSelected)
+    .then(result => {
+        // console.log("promise all results", result) // 'two'
+        const eventsList = result.flat();
+        updateEventElements(eventsList);
+    });
 };
+
+// child function to update DOM elements created by getEventsData
+const updateEventElements = (eventsList) => {
+    const shuffledEvents = shuffle(eventsList);
+    console.log("shuffled events", eventsList);
+    const eventAncestor = document.getElementById('eventsContainer');
+    eventAncestor.innerHTML = '';
+    shuffledEvents.map(function(event) {
+        const eventParent = document.createElement('div');
+        eventParent.classList.add('box');
+        const eventWrapper = document.createElement('div');
+        eventWrapper.classList.add('content');
+        const eventElement = document.createElement('p');
+        eventElement.classList.add('heading');
+        const eventName = event;
+        eventElement.innerText = eventName;
+        
+        eventWrapper.appendChild(eventElement);
+        eventParent.appendChild(eventWrapper);
+        eventAncestor.appendChild(eventParent);
+    })
+}
+
+// Knuth Shuffle, unbiased shuffle algorithm for arrays
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
 
 // Concert Data
 const getConcerts = (currentCity) => {
     const concertURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=concerts&offset=10&place=${currentCity}`;
-    document.getElementById('concertsList').innerHTML = '';
-    getPredict(concertURL).then(function(concertData) {
+    return getPredict(concertURL).then(function(concertData) {
+        let concertList = [];
         concertData.results.map(function(concert) {    
-            const concertList = document.getElementById('concertsList');
-            const concertListName = document.createElement('li');
-            concertListName.innerText = concert.title;
-            concertList.appendChild(concertListName);
+            const concertListName = concert.title;
+            concertList = [...concertList, concertListName];
+            return concertList;
         })
+        // console.log("concert list array", concertList);
+        return concertList;
     });
 };
 
 // Sports Data
 const getSports = (currentCity) => {
     const sportURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=sports&offset=10&place=${currentCity}`;
-    document.getElementById('sportsList').innerHTML = '';
-    getPredict(sportURL).then(function(sportData) {
+    return getPredict(sportURL).then(function(sportData) {
+        let sportList = [];
         sportData.results.map(function(sport) {    
-            const sportList = document.getElementById('sportsList');
-            const sportListName = document.createElement('li');
-            sportListName.innerText = sport.title;
-            sportList.appendChild(sportListName);
+            const sportListName = sport.title;
+            sportList = [...sportList, sportListName];
+            return sportList;
         })
+        // console.log("sport list array", sportList);
+        return sportList;
     });
 };
 
 // Expos Data
 const getExpos = (currentCity) => {
     const expoURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=expos&offset=10&place=${currentCity}`;
-    document.getElementById('exposList').innerHTML = '';
-    getPredict(expoURL).then(function(expoData) {
+    return getPredict(expoURL).then(function(expoData) {
+        let expoList = [];
         expoData.results.map(function(expo) {    
-            const expoList = document.getElementById('exposList');
-            const expoListName = document.createElement('li');
-            expoListName.innerText = expo.title;
-            expoList.appendChild(exposListName);
+            const expoListName = expo.title;
+            expoList = [...expoList, expoListName]
+            return expoList;
         })
+        // console.log("expo list array", expoList);
+        return expoList;
     });
 };
-
-
 
 // Community Data
 const getCommunity = (currentCity) => {
     const communityURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=community&offset=10&place=${currentCity}`;
-    document.getElementById('communityList').innerHTML = '';
-    getPredict(communityURL).then(function(communityData) {
+    return getPredict(communityURL).then(function(communityData) {
+        let communityList = [];
         communityData.results.map(function(community) {    
-            const communityList = document.getElementById('communityList');
-            const communityListName = document.createElement('li');
-            communityListName.innerText = community.title;
-            communityList.appendChild(communityListName);
+            const communityListName = community.title;
+            communityList = [...communityList, communityListName];
+            return communityList;
         })
+        return communityList;
     });
 };
-
 
 // Festivals Data
 const getFestivals = (currentCity) => {
     const festivalURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=festivals&offset=10&place=${currentCity}`;
-    document.getElementById('festivalsList').innerHTML = '';
-    getPredict(festivalURL).then(function(festivalData) {
-        if (festivalData.results != '') {
-            const festivalList = festivalData.results;
-            festivalList.map(function(festival) {
-                const festivalList = document.getElementById('festivalsList');
-                const festivalListName = document.createElement('li');
-                festivalListName.innerText = festival.title;
-                festivalList.appendChild(festivalListName);
-            })
-        } else {
-            const festivalList = document.getElementById('festivalsList');
-            const festivalListName = document.createElement('p');
-            festivalListName.innerText = "No Festivals Scheduled";
-            festivalList.appendChild(festivalListName);
-        }
-    })
+    return getPredict(festivalURL).then(function(festivalData) {
+        let festivalList = [];
+        festivalData.results.map(function(festival) {
+            const festivalListName = festival.title;
+            festivalList = [...festivalList, festivalListName];
+            return festivalList;
+        })
+        // console.log("festival list array", festivalList)
+        return festivalList;
+    });
 };
 
 // Performing-arts
 const getPerformingArts = (currentCity) => {
     const performingArtsURL = `https://api.predicthq.com/v1/events/?active.gte=2020-08-27&category=performing-arts&offset=10&place=${currentCity}`;
-    document.getElementById('performing-artsList').innerHTML = '';
-    getPredict(performingArtsURL).then(function(performingArtsData) {
+    return getPredict(performingArtsURL).then(function(performingArtsData) {
+        let performingList = [];
         performingArtsData.results.map(function(performingArts) {    
-            const performingArtsList = document.getElementById('performing-artsList');
-            const performingArtsListName = document.createElement('li');
-            performingArtsListName.innerText = performingArts.title;
-            performingArtsList.appendChild(performingArtsListName);
+            const performingListName = performingArts.title;
+            performingList = [...performingList, performingListName];
+            return performingList;
         })
+        return performingList;
     });
 };
-
 
 // Craft Beer Data
 const getBreweries = (currentCity) => {
     const breweryURL = `https://api.openbrewerydb.org/breweries?by_city=${currentCity}`;
-    document.getElementById('breweriesList').innerHTML = '';
-    get(breweryURL).then(function(fetchResponse) {
-        fetchResponse.map(function(brewery) {
-            const breweryList = document.getElementById('breweriesList')
-            const breweryListName = document.createElement('li');
-            breweryListName.innerText = brewery.name;
-            breweryList.appendChild(breweryListName);
+    return get(breweryURL).then(function(breweryData) {
+        let breweryList = [];
+        breweryData.map(function(brewery) {
+            const breweryListName = brewery.name;
+            breweryList = [...breweryList, breweryListName];
+            return breweryList;
         })
+        // console.log("brewery list array", breweryList);
+        return breweryList
     });
 };
+
+//check boxes
+let resultButton = document.getElementById('resultButton');
+let result = document.getElementById('resultPlaceholder');
+
+resultButton.addEventListener('click', () => {
+    let checkbox = document.querySelector('input[type="checkbox"]:checked')
+    console.log(alert, "buttonclicked");
+    result.innerText = checkbox.value;
+});
+
+//back up APIs
