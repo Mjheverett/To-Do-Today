96e2697492acc2402849cbf2d6e27522f49ac5ee
Matthew Everett <matthew@mjheverett.com>
2020-09-01 16:00:13 -0400

    uncomment covid modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
-    // modal.style.display = "block";
+    modal.style.display = "block";
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
